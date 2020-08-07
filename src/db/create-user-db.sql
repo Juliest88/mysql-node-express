2f8674cace78ef9c2cdafc4bd902150c302fb7cd
Julia Strichash <julia.s@webpals.com>
2020-08-07 18:24:14 +0300

    A

--- src/db/create-user-db.sql
+++ src/db/create-user-db.sql
@@ -12,6 +12,6 @@ CREATE TABLE IF NOT EXISTS user
      first_name VARCHAR(50) NOT NULL, 
      last_name  VARCHAR(50) NOT NULL, 
      email      VARCHAR(100) UNIQUE NOT NULL, 
-     role       ENUM('admin', 'superuser') DEFAULT 'superuser', 
+     role       ENUM('Admin', 'SuperUser') DEFAULT 'SuperUser', 
      age        INT(11) DEFAULT 0 
   ); 
