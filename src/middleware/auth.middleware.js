c136406200345c30b3738715a9df2dbf6cff80e9
Julia Strichash <julia.s@webpals.com>
2020-08-08 15:23:17 +0300

    a

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,29 +4,6 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// const auth = async (req, res, next) => {
-//     try {
-//         const authHeader = req.headers.authorization;
-
-//         if (!authHeader) {
-//             throw new HttpException(401, 'Access denied. No credentials sent!');
-//         }
-
-//         const token = authHeader.split(' ')[1];
-//         const secretKey = process.env.SECRET_JWT || "";
-
-//         // Verify Toekn
-//         const decoded = jwt.verify(token, secretKey);
-//         const user = await UserModel.findOne({ id: decoded.user_id });
-
-//         req.user = user;
-//         next();
-//     } catch (e) {
-//         e.status = 401;
-//         next(e);
-//     }
-// };
-
 const auth = (...roles) => {
     return async function (req, res, next) {
         try {
@@ -59,3 +36,28 @@ const auth = (...roles) => {
 }
 
 module.exports = auth;
+
+
+
+// const auth = async (req, res, next) => {
+//     try {
+//         const authHeader = req.headers.authorization;
+
+//         if (!authHeader) {
+//             throw new HttpException(401, 'Access denied. No credentials sent!');
+//         }
+
+//         const token = authHeader.split(' ')[1];
+//         const secretKey = process.env.SECRET_JWT || "";
+
+//         // Verify Toekn
+//         const decoded = jwt.verify(token, secretKey);
+//         const user = await UserModel.findOne({ id: decoded.user_id });
+
+//         req.user = user;
+//         next();
+//     } catch (e) {
+//         e.status = 401;
+//         next(e);
+//     }
+// };
