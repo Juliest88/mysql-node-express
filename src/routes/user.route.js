5ade2d261722725f7310703a67598128d1c8cb49
Julia Strichash <julia.s@webpals.com>
2020-08-08 15:36:30 +0300

    A

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,9 +6,9 @@ const Role = require('../utils/role');
 const { createUserSchema, updateUserSchema, validateLogin } = require('../middleware/validators/userValidator.middleware');
 
 
-router.get('/', auth(Role.Admin, Role.SuperUser), userController.getAllUsers); // localhost:3000/api/v1/users
-router.get('/id/:id', auth(Role.Admin, Role.SuperUser), userController.getUserById); // localhost:3000/api/v1/users/id/1
-router.get('/username/:username', auth(Role.Admin, Role.SuperUser), userController.getUserByuserName); // localhost:3000/api/v1/usersname/julia
+router.get('/', auth(), userController.getAllUsers); // localhost:3000/api/v1/users
+router.get('/id/:id', auth(), userController.getUserById); // localhost:3000/api/v1/users/id/1
+router.get('/username/:username', auth(), userController.getUserByuserName); // localhost:3000/api/v1/usersname/julia
 router.post('/', auth(), createUserSchema, userController.createUser); // localhost:3000/api/v1/users
 router.patch('/id/:id', auth(Role.Admin), updateUserSchema, userController.updateUser); // localhost:3000/api/v1/usersid/1 , using patch for partial update
 router.delete('/id/:id', auth(Role.Admin), userController.deleteUser); // localhost:3000/api/v1/usersid/1
