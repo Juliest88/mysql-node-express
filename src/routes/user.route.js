19e7d35917effa0edf3b90cf6c9376dc7e2bf100
Julia Strichash <julia.s@webpals.com>
2020-08-08 15:25:46 +0300

    a

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,11 +7,11 @@ const { createUserSchema, updateUserSchema, validateLogin } = require('../middle
 
 
 router.get('/', auth(Role.Admin, Role.SuperUser), userController.getAllUsers); // localhost:3000/api/v1/users
-router.get('/id/:id', auth(), userController.getUserById); // localhost:3000/api/v1/users/id/1
-router.get('/username/:username', auth(), userController.getUserByuserName); // localhost:3000/api/v1/usersname/julia
+router.get('/id/:id', auth(Role.Admin, Role.SuperUser), userController.getUserById); // localhost:3000/api/v1/users/id/1
+router.get('/username/:username', auth(Role.Admin, Role.SuperUser), userController.getUserByuserName); // localhost:3000/api/v1/usersname/julia
 router.post('/', auth(), createUserSchema, userController.createUser); // localhost:3000/api/v1/users
-router.patch('/id/:id', auth(), updateUserSchema, userController.updateUser); // localhost:3000/api/v1/usersid/1 , using patch for partial update
-router.delete('/id/:id', auth(), userController.deleteUser); // localhost:3000/api/v1/usersid/1
+router.patch('/id/:id', auth(Role.Admin), updateUserSchema, userController.updateUser); // localhost:3000/api/v1/usersid/1 , using patch for partial update
+router.delete('/id/:id', auth(Role.Admin), userController.deleteUser); // localhost:3000/api/v1/usersid/1
 
 
 router.post('/login', validateLogin, userController.userLogin); // localhost:3000/api/v1/users/login
