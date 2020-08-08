38bf1e070bbe8e5d7f8012218b713e57334cd3ae
Julia Strichash <julia.s@webpals.com>
2020-08-08 11:13:01 +0300

    a

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,27 +4,68 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const auth = async (req, res, next) => {
-    try {
-        const authHeader = req.headers.authorization;
+// const auth = async (req, res, next) => {
+//     try {
+//         const authHeader = req.headers.authorization;
 
-        if (!authHeader) {
-            throw new HttpException(401, 'Access denied. No credentials sent!');
+//         if (!authHeader) {
+//             throw new HttpException(401, 'Access denied. No credentials sent!');
+//         }
+
+//         const token = authHeader.split(' ')[1];
+//         const secretKey = process.env.SECRET_JWT || "";
+
+//         // Verify Toekn
+//         const decoded = jwt.verify(token, secretKey);
+//         const user = await UserModel.findOne({ id: decoded.user_id });
+
+//         req.user = user;
+//         next();
+//     } catch (e) {
+//         e.status = 401;
+//         next(e);
+//     }
+// };
+
+
+// const hasRole = (...roles) => {
+//     return function (req, res, next) {
+//         if (roles.length && !roles.includes(req.user.role)) {
+//             throw new HttpException(401, 'Unauthorized');
+//         }
+
+//         next();
+//     }
+// }
+
+const auth = (...roles) => {
+    return async function (req, res, next) {
+        try {
+            const authHeader = req.headers.authorization;
+
+            if (!authHeader) {
+                throw new HttpException(401, 'Access denied. No credentials sent!');
+            }
+
+            const token = authHeader.split(' ')[1];
+            const secretKey = process.env.SECRET_JWT || "";
+
+            // Verify Toekn
+            const decoded = jwt.verify(token, secretKey);
+            const user = await UserModel.findOne({ id: decoded.user_id });
+
+            if (roles.length && !roles.includes(user.role)) {
+                throw new HttpException(401, 'Unauthorized');
+            }
+
+            req.user = user;
+            next();
+
+        } catch (e) {
+            e.status = 401;
+            next(e);
         }
-
-        const token = authHeader.split(' ')[1];
-        const secretKey = process.env.SECRET_JWT || "";
-
-        // Verify Toekn
-        const decoded = jwt.verify(token, secretKey);
-        const user = await UserModel.findOne({ id: decoded.user_id });
-
-        req.user = user;
-        next();
-    } catch (e) {
-        e.status = 401;
-        next(e);
     }
-};
+}
 
 module.exports = auth;
