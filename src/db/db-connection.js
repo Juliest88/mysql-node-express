a50ff5d95422c9b1c257930cb00530e537ce4b15
Julia Strichash <julieza88@gmail.com>
2025-08-14 13:25:19 +0300

    fix: use correct env variable DB_PASSWORD instead of DB_PASS in db-connection.js

--- src/db/db-connection.js
+++ src/db/db-connection.js
@@ -7,7 +7,7 @@ class DBConnection {
         this.db = mysql2.createPool({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
-            password: process.env.DB_PASS,
+            password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE
         });
 
